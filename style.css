44ea85f0f110a8b0ab2d0f301196b2998b882ebc
kdsekar <kd.sekarayu1234@gmail.com>
2022-04-13 01:14:32 +0800

    desain tampilan box pada web

---
+++ style.css
@@ -0,0 +1,14 @@
+#box{
+    display: flex;
+    height: 100px;
+    width: 100px;
+    position: absolute;
+    top: 80%;
+    left: 50%;
+    background-color:black;
+    bottom: 50px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+    
+}
